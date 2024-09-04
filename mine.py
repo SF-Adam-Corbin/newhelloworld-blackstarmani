d8dfde2bd359ce0d047bafb5dcdfb6b682c2389a
blackstarmani <154953899+blackstarmani@users.noreply.github.com>
2024-09-03 22:12:34 -0400

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,10 +1,11 @@
 # Write a line of code that prints "Hello world!".
 print("Hello world!")
 
+print(hello_world())
 
 def hello_world():
   # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
   variable_name = "Hello world!"
-  print(hello_world())
+  
   return variable_name
   
