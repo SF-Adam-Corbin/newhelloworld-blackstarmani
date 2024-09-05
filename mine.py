ad2e6a3935cf2659ae4e0adce3882718bb1b93ec
blackstarmani <154953899+blackstarmani@users.noreply.github.com>
2024-09-05 09:12:39 -0400

    Update mine.py
    Co-authored-by: Adam Corbin <acorbin3@gmail.com>

--- mine.py
+++ mine.py
@@ -6,7 +6,6 @@ print("Hello world!")
 def hello_world():
   # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
   variable_name = "Hello world!"
-  print(variable_name)
   return variable_name
 
 hello_world()
